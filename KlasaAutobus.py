9a5675f4dec1fcb0aff48b61e5b9e38b3ac24dfa
Łucja Tarnowska <lucja.tarnowska@student.wat.edu.pl>
2024-06-08 20:55:09 +0200

    Dodanie okienta edycji autobusow

--- KlasaAutobus.py
+++ KlasaAutobus.py
@@ -1,5 +1,35 @@
+from tkinter import ttk 
+from tkinter import * 
+
+
 class KlasaAutobus:
-    def __init__(self, nazwa_autobusu, lokalizacja, wsp_x, wsp_y):
+    def dodawanie_autobusu_okno(self):
+        self.root = Tk()
+        self.root.resizable(False, False)
+        self.root.title("Autobusy w Kobylce - dodawanie autobusu")
+        self.root.geometry("600x200")
+
+        label_tytul = Label(self.root, text="Dodawanie autobusu")
+        label_nazwa_autobusu = Label(self.root, text="Podaj nazwę autobusu: ")
+        label_lokalizacja_autobusu = Label(self.root, text="Podaj lokalizację autobusu: ")
+
+        self.nazwa_autobusu = Entry(self.root, width=50)
+        self.lokalizacja = Entry(self.root, width=50)
+        
+        label_tytul.grid(row=0, column=0, columnspan=2)
+        label_nazwa_autobusu.grid(row=1, column=0)
+        label_lokalizacja_autobusu.grid(row=2, column=0)
+
+        self.nazwa_autobusu.grid(row=1, column=1)
+        self.lokalizacja.grid(row=2, column=1)
+
+        button_dodaj_autobus = Button(self.root, text="Dodaj autobus")
+        button_dodaj_autobus.grid(row=3, column=1)
+
+        self.root.mainloop()
+    
+
+    def aktualizuj_autobus(self, nazwa_autobusu, lokalizacja, wsp_x, wsp_y):
         self.nazwa_autobusu = nazwa_autobusu
         self.lokalizacja = lokalizacja
         self.wsp_x = wsp_x
