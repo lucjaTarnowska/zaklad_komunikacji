6fbbb6a0f61f1c40833c9740fa873f457661560a
Łucja Tarnowska <lucja.tarnowska@student.wat.edu.pl>
2024-06-09 10:37:24 +0200

    Dodanie wszystkich wymaganych funkcjonalności do klas z polecenia

--- KlasaAutobus.py
+++ KlasaAutobus.py
@@ -1,36 +1,47 @@
-from tkinter import ttk 
 from tkinter import * 
 
+from Lokalizacja import miejsce_na_wspolrzedne
 
 class KlasaAutobus:
-    def dodawanie_autobusu_okno(self):
+    def __init__(self):
+        self.nazwa_autobusu = ""
+        self.lokalizacja = ""
+        self.wsp_x = 0
+        self.wsp_y = 0
+    
+
+    def okno_edycji_autobusu(self):
         self.root = Tk()
         self.root.resizable(False, False)
-        self.root.title("Autobusy w Kobylce - dodawanie autobusu")
+        self.root.title("Autobusy w Kobylce - autobusy")
         self.root.geometry("600x200")
 
-        label_tytul = Label(self.root, text="Dodawanie autobusu")
+        label_tytul = Label(self.root, text="Uzupelnij dane autobusu")
         label_nazwa_autobusu = Label(self.root, text="Podaj nazwę autobusu: ")
         label_lokalizacja_autobusu = Label(self.root, text="Podaj lokalizację autobusu: ")
 
-        self.nazwa_autobusu = Entry(self.root, width=50)
-        self.lokalizacja = Entry(self.root, width=50)
+        self.pole_nazwa_autobusu = Entry(self.root, width=50)
+        self.pole_lokalizacja_autobusu = Entry(self.root, width=50)
         
         label_tytul.grid(row=0, column=0, columnspan=2)
         label_nazwa_autobusu.grid(row=1, column=0)
         label_lokalizacja_autobusu.grid(row=2, column=0)
 
-        self.nazwa_autobusu.grid(row=1, column=1)
-        self.lokalizacja.grid(row=2, column=1)
+        self.pole_nazwa_autobusu.grid(row=1, column=1)
+        self.pole_lokalizacja_autobusu.grid(row=2, column=1)
 
-        button_dodaj_autobus = Button(self.root, text="Dodaj autobus")
+        self.pole_nazwa_autobusu.insert(0, self.nazwa_autobusu)
+        self.pole_lokalizacja_autobusu.insert(0, self.lokalizacja)
+
+        button_dodaj_autobus = Button(self.root, text="Zapisz dane", command=self.aktualizuj_autobus)
         button_dodaj_autobus.grid(row=3, column=1)
-
         self.root.mainloop()
-    
 
-    def aktualizuj_autobus(self, nazwa_autobusu, lokalizacja, wsp_x, wsp_y):
-        self.nazwa_autobusu = nazwa_autobusu
-        self.lokalizacja = lokalizacja
-        self.wsp_x = wsp_x
-        self.wsp_y = wsp_y
+    def aktualizuj_autobus(self):
+        self.nazwa_autobusu = self.pole_nazwa_autobusu.get()
+        self.lokalizacja = self.pole_lokalizacja_autobusu.get()
+        koordynaty = miejsce_na_wspolrzedne(self.pole_lokalizacja_autobusu.get())
+        self.wsp_x = koordynaty[0]
+        self.wsp_y = koordynaty[1]
+        self.root.destroy()
+        self.root.quit()
--- KlasaKlient.py
+++ KlasaKlient.py
@@ -1,7 +1,75 @@
+from tkinter import ttk 
+from tkinter import * 
+
+from Lokalizacja import miejsce_na_wspolrzedne
+
+
 class KlasaKlient:
-    def __init__(self, imie, nazwisko, lokalizacja, wsp_x, wsp_y):
-        self.imie = imie
-        self.nazwisko = nazwisko
-        self.lokalizacja = lokalizacja
-        self.wsp_x = wsp_x
-        self.wsp_y = wsp_y
+    def __init__(self):
+        self.imie = ""
+        self.nazwisko = ""
+        self.lokalizacja = ""
+        self.autobus = ""
+        self.wsp_x = 0
+        self.wsp_y = 0
+    
+
+    def okno_edycji_klienta(self, lista_autobusow):
+        lista_do_wyboru_autobusow = []
+
+        for aktualny_autobus in lista_autobusow:
+            lista_do_wyboru_autobusow.append(aktualny_autobus.nazwa_autobusu)
+
+        self.root = Tk()
+        self.root.resizable(False, False)
+        self.root.title("Autobusy w Kobylce - klient")
+        self.root.geometry("600x200")
+
+        label_tytul = Label(self.root, text="Uzupelnij dane klienta")
+        label_imie = Label(self.root, text="Podaj imie klienta: ")
+        label_nazwisko = Label(self.root, text="Podaj nazwisko klienta: ")
+        label_lokalizacja = Label(self.root, text="Podaj lokalizację klienta: ")
+        label_autobus = Label(self.root, text="Wybierz autobus: ")
+
+        self.pole_imie = Entry(self.root, width=50)
+        self.pole_nazwisko = Entry(self.root, width=50)
+        self.pole_lokalizacja = Entry(self.root, width=50)
+        self.wybor_autobusow = ttk.Combobox(self.root, state="readonly",  values=lista_do_wyboru_autobusow, width=47)
+        
+        label_tytul.grid(row=0, column=0, columnspan=2)
+        label_imie.grid(row=1, column=0)
+        label_nazwisko.grid(row=2, column=0)
+        label_lokalizacja.grid(row=3, column=0)
+        label_autobus.grid(row=4, column=0)
+
+        self.pole_imie.grid(row=1, column=1)
+        self.pole_nazwisko.grid(row=2, column=1)
+        self.pole_lokalizacja.grid(row=3, column=1)
+        self.wybor_autobusow.grid(row=4, column=1)
+
+        self.pole_imie.insert(0, self.imie)
+        self.pole_nazwisko.insert(0, self.nazwisko)
+        self.pole_lokalizacja.insert(0, self.lokalizacja)
+
+        if self.autobus:
+            for idx,  aktualny_autobus in enumerate(lista_do_wyboru_autobusow):
+                if aktualny_autobus == self.autobus:
+                    self.wybor_autobusow.current(idx)
+                    break
+
+        button_zapisz_klienta = Button(self.root, text="Zapisz dane", command=self.aktualizuj_klienta)
+        button_zapisz_klienta.grid(row=5, column=1)
+        self.root.mainloop()
+
+
+    def aktualizuj_klienta(self):
+        self.imie = self.pole_imie.get()
+        self.nazwisko = self.pole_nazwisko.get()
+        self.lokalizacja = self.pole_lokalizacja.get()
+        self.autobus = self.wybor_autobusow.get()
+
+        koordynaty = miejsce_na_wspolrzedne(self.pole_lokalizacja.get())
+        self.wsp_x = koordynaty[0]
+        self.wsp_y = koordynaty[1]
+        self.root.destroy()
+        self.root.quit()
