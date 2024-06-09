977f6a9652a3d2e557aa4beeb480982987435195
Łucja Tarnowska <lucja.tarnowska@student.wat.edu.pl>
2024-06-10 00:41:15 +0200

    Poprawa odpytywania o adresy

--- Lokalizacja.py
+++ Lokalizacja.py
@@ -1,8 +1,17 @@
 # Dany plik służy do zagregowania funkcji odpowiedzialnych za zamianę współrzędnych na adres i w drugą stronę
-
+import ssl # Biblioteka do certyfikatów stron internetowych HTTPS.
+import geopy
 from geopy.geocoders import Nominatim # Biblioteka do odytywania o miejsca i współrzędne.
 
-geolocator = Nominatim(user_agent="Mozilla/5.0") # Ustawienie agenta w nagłówku. Wymaga tego dana biblioteka.
+# Dany obiekt służy do stworzenia kontekstu SSL czyli zabezpieczeń witryn internetowych.
+# W nowym kontekście wyłączamy sprawdzanie certyfikatów. Dany kontekst zostanie użyty do biblioteki Nominatim.
+# Jest on niezbędny aby uniknąć błędów certyfikatu podczas odpytywania o adresy.
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+# Ustawienie agenta w nagłówku. Wymaga tego dana biblioteka.
+geolocator = Nominatim(user_agent="Mozilla/5.0", ssl_context=ctx, adapter_factory=geopy.adapters.URLLibAdapter) 
 
 
 def miejsce_na_wspolrzedne(miejsce):
